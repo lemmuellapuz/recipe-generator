0f8cf63a8af22835bddc1ab1ed98cfbe70a41e2f
unknown <lapuz.lemmuel@gmail.com>
2025-02-07 18:08:08 +0800

    chore(spoonacular): add spoonacular config file and update .env.example

---
+++ config/spoonacular.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Spoonacular
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the key of spoonacular API
+    |
+    */
+
+    'secret_key' => env('SPOONACULAR_SECRET'),
+
+];
