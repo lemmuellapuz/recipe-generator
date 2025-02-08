d6b98461542a4220c554bfc26693f1daebb61f18
unknown <lapuz.lemmuel@gmail.com>
2025-02-08 15:39:48 +0800

    fix(api): Fix recipe controller in routes api

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,9 @@
 <?php
 
-use App\Http\Controllers\API\GenerateRecipeController;
+use App\Http\Controllers\API\RecipeController;
 use App\Http\Controllers\API\InstructionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/generate-recipe', GenerateRecipeController::class);
+Route::post('/generate-recipe', RecipeController::class);
 Route::get('/show-instructions/{id}', InstructionController::class);
