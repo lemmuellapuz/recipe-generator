179acd11a43eb6e2e159c9aab752c2e5901a7829
unknown <lapuz.lemmuel@gmail.com>
2025-02-08 15:46:30 +0800

    chore(config): add new configuration file of hugging face ai for image recognition

---
+++ config/huggingface.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Spoonacular
+    |--------------------------------------------------------------------------
+    |
+    | This file is for storing the key of spoonacular API
+    |
+    */
+
+    'access_token' => env('HUGGING_FACE_ACCESS_TOKEN'),
+
+];
