83ce0cc2f466aa8768442a82fd1bc909f1278866
unknown <lapuz.lemmuel@gmail.com>
2025-02-08 17:05:38 +0800

    feat(image-recognition): added api route for image recognition

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Http\Controllers\API\ImageRecognitionController;
 use App\Http\Controllers\API\RecipeController;
 use App\Http\Controllers\API\InstructionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+Route::post('/analyze-image', ImageRecognitionController::class);
 Route::post('/generate-recipe', RecipeController::class);
 Route::get('/show-instructions/{id}', InstructionController::class);
