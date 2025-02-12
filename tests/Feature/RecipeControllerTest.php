05cd2d5509cf65078ac7e97683867d5214633b37
unknown <lapuz.lemmuel@gmail.com>
2025-02-12 10:29:41 +0800

    test(recipecontroller): add additional test cases for successful returns

--- tests/Feature/RecipeControllerTest.php
+++ tests/Feature/RecipeControllerTest.php
@@ -55,11 +55,61 @@ class RecipeControllerTest extends TestCase
     public function test_it_returns_200_status_code_when_ingredients_are_array(): void
     {
 
+        Http::fake([
+            'https://api.spoonacular.com/*' => Http::response($this->getMockedRecipeResponse(), 200)
+        ]);
+
         $response = $this->postJson($this->url, [
-            'ingredients' => ['tomato', 'cheese']
+            'ingredients' => ['tomato']
         ]);
     
         $response->assertStatus(200);
+    }
+
+    public function test_it_returns_recipe_with_matched_ingredients(): void
+    {
+        Http::fake([
+            'https://api.spoonacular.com/*' => Http::response($this->getMockedRecipeResponse(), 200)
+        ]);
+
+        $response = $this->postJson($this->url, [
+            'ingredients' => ['tomato']
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonFragment(['name' => 'Tomato']);
+    }
+
+    private function getMockedRecipeResponse(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Tomato Recipe',
+                'image' => 'https://tomatosalad.com/tomatosalad.jpg',
+                'imageType' => 'jpg',
+                'usedIngredientCount' => 1,
+                'missedIngredientCount' => 0,
+                'missedIngredients' => [],
+                'usedIngredients' => [
+                    [
+                        'id' => 1,
+                        'amount' => '5',
+                        'unit' => 'kg',
+                        'unitLong' => 'kilogram',
+                        'unitShort' => 'kg',
+                        'aisle' => '',
+                        'name' => 'Tomato',
+                        'original' => 'Tomato',
+                        'originalName' => 'Tomato',
+                        'meta' => '',
+                        'image' => 'https://tomato.com/tomato.jpg'
+                    ],
+                ],
+                'unusedIngredients' => [],
+                'likes' => 15
+            ]
+        ];
     }
 
 }
